9f93babbfad2d560dffb4a5139f8362ad78662dc
tannerwolff <twolff@dawson.edu>
2021-02-27 12:51:49 -0800

    initial commit

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Action of Eastern Montana</h1>
+        <h2>Hi there</h2>
+      </div>
+    );
+  }
+}
