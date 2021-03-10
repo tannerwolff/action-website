ec426d5fa56bb4845263a1b9ec8f28111e2b7bed
tannerwolff <twolff@dawson.edu>
2021-03-10 16:33:09 -0700

    Initial testing commit second runthrough

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import moment from "moment";
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>Action of Eastern Montana</h1>
-        <h2>Hi there</h2>
+        <div>
+          {moment().format('MMMM Do YYYY, h:mm:ss a')}
+        </div>
       </div>
     );
   }
